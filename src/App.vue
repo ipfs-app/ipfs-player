286f83dd83a1eb0e24fcee994c54a31f0cc9e269
bill080307 <bill080307@live.cn>
2024-05-04 20:01:06 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,20 @@
     </div>
     <div class="row">
       <div class="col">
-        <h1>{{ title }}</h1>
+        <h1>
+          {{ title }}
+          <span class="fs-3" v-if="! single">{{ num }}</span>
+        </h1>
       </div>
     </div>
     <div class="row">
-      <div class="col-8">
+      <div :class="single?'col-12':'col-8'">
         <p>{{ description }}</p>
       </div>
-      <div class="col-4">
-        <button type="button" class="btn btn-outline-light multiple" v-for="(item,index) in filelist" data-bs-toggle="tooltip" :data-bs-title="item.title" @click="play(index)">
-          {{ padding(index + 1, Math.log(filelist.length) / Math.log(10))}}
+      <div class="col-4" v-if="! single">
+        <button type="button" class="btn btn-outline-light multiple" v-for="(item,index) in filelist"
+                data-bs-toggle="tooltip" :data-bs-title="item.title" @click="play(index)">
+          {{ padding(index + 1, Math.log(filelist.length) / Math.log(10)) }}
         </button>
       </div>
     </div>
@@ -35,8 +39,11 @@ onMounted(() => {
 })
 let cover = ""
 const title = ref("")
+const num = ref("")
 const description = ref("")
 const filelist = ref([])
+const single = ref(true)
+
 async function init() {
   let hash = window.location.hash;
   let files_json = "./files.json"
@@ -51,15 +58,24 @@ async function init() {
     }
   }
   await Axios.get(files_json).then((res) => {
-    cover = res.data.cover
+    cover = res.data["cover"]
     title.value = res.data.title
     description.value = res.data.description
-    play_video(res.data.files[0])
     filelist.value = res.data.files
+    if (filelist.value.length > 1) {
+      single.value = false
+      num.value = padding(1, Math.log(filelist.value.length) / Math.log(10))
+    }
+    play_video(res.data.files[0])
   });
 }
 
 function play_video(video_file) {
+  if (single.value) {
+    document.title = title.value + " IPFS Player"
+  } else {
+    document.title = title.value + " " + num.value + " IPFS Player"
+  }
   new xgplayer({
     id: 'player',
     url: video_file.url,
@@ -70,22 +86,26 @@ function play_video(video_file) {
     poster: cover,
   });
 }
+
 function play(item) {
+  num.value = padding(item + 1, Math.log(filelist.value.length) / Math.log(10))
   play_video(filelist.value[item])
 }
+
 function padding(num, length) {
-  for(let len = (num + "").length; len < length; len = num.length) {
-   num = "0" + num;
+  for (let len = (num + "").length; len < length; len = num.length) {
+    num = "0" + num;
   }
   return num;
- }
+}
 </script>
 <style scoped>
 #player {
   width: 1296px;
   height: 729px;
 }
-.multiple{
+
+.multiple {
   margin: 0.2em;
 }
 </style>
